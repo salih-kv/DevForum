38087e347e3f70220201e2b3d04976f69af96385
salih-kv <profileofsalih@gmail.com>
2024-03-06 11:16:32 +0530

    add shadcn/ui and navbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,6 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div className="h-screen">
-      <header className="border-b p-4">
-        <UserButton />
-      </header>
       <div className="flex items-center justify-center p-8">
         <p>Home</p>
       </div>
